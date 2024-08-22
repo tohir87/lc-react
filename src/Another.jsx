7f85f57017f8eb4d2efb1712f73b155ab746f2e1
Tohir Omoloye <otcleantech@gmail.com>
2024-08-22 17:35:03 +0100

    Testing

---
+++ src/Another.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+export default function Another(props) {
+	return (
+		<div>
+		<h1>Another Component: {props.name}</h1>
+		</div>
+	)
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
+import Another from './Another';
 import './App.css';
+import { useState, useCallback } from 'react';
 
 function App() {
+	const [count, setCount] = useState(0)
+
+	function decrement() {
+		setCount(count - 1)
+	}
+
+	function increment() {
+		setCount(count + 1)
+	}
+
   return (
     <div className="App">
       <header className="App-header">
+	<Another name="James" />
+		<div>
+			<span>{count}</span>
+		
+				<button onClick={increment}>+</button>
+
+			<button onClick={decrement}>-</button>
+		</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
